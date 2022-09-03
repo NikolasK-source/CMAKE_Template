8d7665a2dc79634a793eede528f10040c9a4b988
Nikolas Koesling <nikolas@koesling.info>
2022-09-03 15:58:38 +0200

    test target

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,14 @@
  */
 
 #include <iostream>
+#include <omp.h>
 
 int main() {
     // TODO implement
     std::cout << PROJECT_NAME << ' ' << PROJECT_VERSION << ' ' << "Hello CMake!!!" << std::endl;
+
+#ifdef _OPENMP
+#    pragma omp parallel default(none)
+    { printf("Hello from thread: %d\n", omp_get_thread_num()); }
+#endif
 }
